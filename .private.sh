4a1a87c08032b333f1a30b03f9fbb1a34a62a3c6
root <root@EL7.tsc>
2024-05-06 10:42:50 +0800

    tmp

---
+++ .private.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set +o posix
+
+declare -a EL7__x86_64 EL7__aarch64 FHOS__x86_64 FHOS__aarch64
+# os arch ksfile
+EL7__x86_64=("EL7" "x86_64" "EL7.ks")
+EL7__aarch64=("EL7" "aarch64" "EL7.ks")
+FHOS__x86_64=("FHOS" "x86_64" "FHOS.ks")
+FHOS__aarch64=("FHOS" "aarch64" "FHOS.ks")
+
+declare -A requirements
+# requirements=("ipcalc" "systemd-nspawn" "ifconfig" "screen")
+requirements=(
+    ["lsof"]="lsof"
+    ["screen"]="screen"
+    ["ipcalc"]="initscripts"
+    ["ifconfig"]="net-tools"
+    ["systemd-nspawn"]="systemd(EL7)或systemd-container(FHOS)"
+)
+declare -A ports
+# ports=(80 67 69)
+ports=(
+    ['httpd']='TCP:80'
+    ['dhcpd']='UDP:67'
+    ['tftp']='UDP:69'
+)
